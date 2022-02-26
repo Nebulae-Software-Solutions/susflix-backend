ed641174549fe1b424675145cc18813f87c742a3
Gonzalo Muñoz <gzm.def@gmail.com>
2022-02-26 00:23:42 -0300

    Update getAPIinfo.js

--- src/utils/getAPIinfo.js
+++ src/utils/getAPIinfo.js
@@ -18,7 +18,7 @@ const downloadAndSaveMovies = async () => {
         BASE_URL: 'https://yts.am/api/v2/list_movies.json',
         PARAMS: { limit: 50, page: 1, sort_by: "year" },
     }
-    const totalMoviesLimit = 1_000;
+    const totalMoviesLimit = 9_900;
     const totalPagesLimit = totalMoviesLimit / API.PARAMS.limit;
 
     // Averiguamos cuántas películas hay disponibles para descargar
