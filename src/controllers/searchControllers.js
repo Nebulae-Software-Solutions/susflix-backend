8ac9441db6688a02730ac4f7fed5ed4fa30147a5
Gonzalo Muñoz <gzm.def@gmail.com>
2022-03-03 00:26:53 -0300

    included useful extra info in the response

--- src/controllers/searchControllers.js
+++ src/controllers/searchControllers.js
@@ -49,6 +49,7 @@ const get = async (req, res) => {
     if (genre) where.genres = { [Op.overlap]: genre };
     if (rating) where.rating = { [Op.between]: rating };
 
+
     try {
         const movies = await Movie.findAll({
             where,
@@ -58,7 +59,37 @@ const get = async (req, res) => {
             offset,
         })
 
-        res.json(movies);
+        // We need to find out how many movies there are in total:
+        const count = await Movie.count({ where });
+
+        // Auxiliar function to calculate the next and prevous pages based on the number of movies and the limit:
+        const prevAndNext = (page, limit) => {
+
+            const prev = page - 1;
+            const next = page + 1;
+
+            const searchStr = (identifier) => (
+                `/search?${Object.entries({ ...req.query, page: identifier, limit }).map(([key, value]) => `${key}=${value}`).join('&')}`
+            )
+
+            return {
+                prev: prev > 0 ? searchStr(prev) : null,
+                next: next <= Math.ceil(count / limit) ? searchStr(next) : null
+            }
+        }
+
+        const response = {
+            status: 'success',
+            total: count,
+            here: movies.length,
+            page,
+            pages: Math.ceil(count / limit),
+            prev: prevAndNext(page, limit).prev,
+            next: prevAndNext(page, limit).next,
+            data: movies,
+        }
+
+        res.json(response);
     } catch (error) {
         console.log("Something went wrong: ");
         res.status(500).json({ error: error.message });
