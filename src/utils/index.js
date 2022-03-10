c4a4a6cec3a661ee79a89f023dba789cc40ce9a9
M-Gonzalo <gzm.def@gmail.com>
2022-03-10 02:58:16 +0000

    some files were missing

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+const downloadAndSaveMovies = require('./getAPIinfo');
+const paramsNormalization = require('./searchParamsNormalization');
+
+module.exports = {
+    downloadAndSaveMovies,
+    paramsNormalization
+}
