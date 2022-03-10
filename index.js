22e87ab02689b0988264f38fcdf933cba63ab171
Gonzalo Muñoz <gzm.def@gmail.com>
2022-03-10 19:45:45 -0300

    example code

--- index.js
+++ index.js
@@ -13,18 +13,18 @@
 //  █                                                                                     █
 
 
-require('dotenv').config();
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+require('dotenv').config()
+const server = require('./src/app.js')
+const { conn } = require('./src/db.js')
 
-const isHeroku = process.env.DATABASE_URL?.includes('amazonaws');
-const { downloadAndSaveMovies } = require('./src/utils');
+const isHeroku = process.env.DATABASE_URL?.includes('amazonaws')
+const { downloadAndSaveMovies } = require('./src/utils')
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
   const PORT = process.env.PORT || 3001
   server.listen(PORT, () => {
-    console.log('Server listening at 3001'); // eslint-disable-line no-console
-  });
+    console.log('Server listening at 3001') // eslint-disable-line no-console
+  })
   // if (!isHeroku) downloadAndSaveMovies(50_000);
-});
+})
