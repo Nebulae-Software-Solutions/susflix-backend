45cbb7ce5bf204498ecad5f77608b7ede175329c
Gonzalo Muñoz <gzm.def@gmail.com>
2022-08-22 23:53:27 -0300

    more functional style refactoring

--- src/utils/getAPIinfoInParallel.js
+++ src/utils/getAPIinfoInParallel.js
@@ -4,114 +4,99 @@ const axios = require('axios')
 const progressBar = require('progress-barjs')
 
 
+const downloadAndSaveMovies = async (totalMoviesLimit = 5000) => {
 
-// Necesitamos:
-// 1. Obtener número total de películas. Lo vamos a obtener de la primera página, y sirve para mostrar una barra de progreso.
-// 2. Descargar una por una todas las páginas (traen 50 cada una).
-// 3. Guardar en la base de datos.
-
-
-const downloadAndSaveMovies = async (totalMoviesLimit) => {
+    const compose = (...fns) => fns.reduce((f, g) => async (...args) => f(await g(...args)))
 
     const API = {
         BASE_URL: 'https://yts.am/api/v2/list_movies.json',
         PARAMS: { limit: 50, page: 1, sort_by: "date_added" },
     }
-    const totalPagesLimit = totalMoviesLimit / API.PARAMS.limit
 
-    // Averiguamos cuántas películas hay disponibles para descargar
-    const { data } = await axios.get(`${API.BASE_URL}?limit=1`)
-    const totalMoviesInAPI = data.data.movie_count
+    const get = async (url, retries = 25) => {
+        try { return await axios.get(url, { timeout: 2_500 }) }
+        catch {
+            if (retries > 0) { return get(url, retries - 1) }
+            else { console.log(`Error downloading ${url}`) }
+        }
+    }
+    const pick = (obj, parts) => parts.split('.').reduce((o, p) => o[p], obj)
+    const pickMovies = e => pick(e, 'data.data.movies')
+    const pickMovieCount = e => pick(e, 'data.data.movie_count')
+    const sanitize = movies => movies.map(movie => ({
+        id: movie?.id,
+        imdb_code: movie?.imdb_code,
+        title: movie?.title,
+        title_long: movie?.title_long,
+        year: movie?.year,
+        rating: movie?.rating,
+        runtime: movie?.runtime,
+        genres: movie?.genres,
+        synopsis: movie?.synopsis,
+        description_full: movie?.description_full,
+        yt_trailer_code: movie?.yt_trailer_code,
+        language: movie?.language,
+        small_cover_image: movie?.small_cover_image,
+        medium_cover_image: movie?.medium_cover_image,
+        large_cover_image: movie?.large_cover_image,
+        background_image: movie?.background_image,
+        background_image_original: movie?.background_image_original,
+    }))
 
-    // Limite impuesto manualmente, o número total de películas, lo que sea menor:
+    const allMovies = new Set()
+    const dedupe = movies => {
+        const output = movies.filter(movie => !allMovies.has(`${movie.id} - ${movie.title}`))
+        movies.forEach(movie => allMovies.add(`${movie.id} - ${movie.title}`))
+        return output
+    }
+
+    const saveToDB = async movies => await Movie
+        .bulkCreate(movies, { updateOnDuplicate: Object.keys(Movie.tableAttributes) })
+        .then(() => movies)
+
+    const showProgress = movies => {
+        movies.forEach(() => { bar.tick('') })
+    }
+
+    const totalMoviesInAPI = await get(`${API.BASE_URL}?limit=1`).then(pickMovieCount)
     const totalPagesInAPI = Math.ceil(totalMoviesInAPI / API.PARAMS.limit)
-    const totalPages = totalPagesInAPI < totalPagesLimit ? totalPagesInAPI : totalPagesLimit
+    const totalPagesLimit = Math.ceil(totalMoviesLimit / API.PARAMS.limit)
+    const totalPages = totalPagesInAPI < totalPagesLimit
+        ? totalPagesInAPI : totalPagesLimit
+    const totalMovies = totalPages * API.PARAMS.limit < totalMoviesInAPI
+        ? totalPages * API.PARAMS.limit : totalMoviesInAPI
 
-    // Inicializamos la barra de progreso
-    const totalMovies =
-        totalMoviesLimit < totalMoviesInAPI
-            ? totalPages * API.PARAMS.limit
-            : totalMoviesInAPI
-    const progressBarOptions = {
+    const bar = progressBar({
         label: 'Downloading movie info',
         total: totalMovies,
-        show: { overwrite: true }
-    }
-    const bar = progressBar(progressBarOptions)
-
-    // Función auxiliar para filtrar los datos relevantes:
-    const sanitize = (movies) => {
-
-        const filteredData = movies.map(movie => {
-
-            bar.tick(movie.title_long)
-            console.log('')
-
-            return {
-                id: movie.id,
-                imdb_code: movie.imdb_code,
-                title: movie.title,
-                title_long: movie.title_long,
-                year: movie.year,
-                rating: movie.rating,
-                runtime: movie.runtime,
-                genres: movie.genres, // Array
-                synopsis: movie.synopsis,
-                description_full: movie.description_full,
-                yt_trailer_code: movie.yt_trailer_code,
-                language: movie.language,
-                background_image: movie.background_image,
-                background_image_original: movie.background_image_original,
-                small_cover_image: movie.small_cover_image,
-                medium_cover_image: movie.medium_cover_image,
-                large_cover_image: movie.large_cover_image,
+        show: {
+            bar: {
+                length: 75,
+                completed: '*',
+                incompleted: ' '
             }
-        })
-
-        return filteredData
-
-    }
-
-    // Función auxiliar para guardar las películas en la base de datos de a 50 en 50
-    const saveToDB = async (movies) => {
-        Movie.bulkCreate(movies,
-            { updateOnDuplicate: Object.keys(Movie.tableAttributes) }
-        )
-    }
-
-
-    const downloadAndSave = async (pages) => {
-
-        const withTimeout = (millis, promise) => {
-            const timeout = new Promise((resolve, reject) =>
-                setTimeout(
-                    () => reject(`Timed out after ${millis} ms.`),
-                    millis))
-            return Promise.race([
-                promise,
-                timeout
-            ])
         }
-
-        const allPagesPromises = pages.map(async page => {
-
-            return withTimeout(100, axios.get(API.BASE_URL, { params: { ...API.PARAMS, page } })
-                .then(({ data }) => {
-                    const movies = sanitize(data.data.movies)
-                    saveToDB(movies)
-                })
-                .catch(err => {
-                    downloadAndSave([page])
-                })
-            )
-        })
-
-        Promise.allSettled(allPagesPromises)
-    }
+    })
 
     const allPages = Array.from({ length: totalPages }, (v, k) => k + 1)
-    downloadAndSave(allPages)
+    const allPagesPromises = pages => pages.map(async p => {
+        API.PARAMS.page = p
+        await compose(
+            showProgress,
+            saveToDB,
+            dedupe,
+            sanitize,
+            pickMovies,
+            get
+        )(`${API.BASE_URL}?${Object.entries(API.PARAMS).map(([k, v]) => `${k}=${v}`).join('&')}`)
+    })
 
+    for (let i = 0; i < allPages.length; i += 15) {
+        await Promise.allSettled(allPagesPromises(allPages.slice(i, i + 15)))
+    }
+    showProgress(Array.from({ length: totalMovies - allMovies.size }, () => ''))
+
+    console.log(`Downloaded details about ${totalMovies} movies, ${allMovies.size} unique saved to the database.`)
 }
 
 
