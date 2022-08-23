4b0ed73858f821e3ab30aa39104a6ed0ee0403b6
Gonzalo Muñoz <gzm.def@gmail.com>
2022-08-22 23:50:39 -0300

    refactoring

--- index.js
+++ index.js
@@ -16,18 +16,18 @@
 require('dotenv').config()
 const server = require('./src/app.js')
 const {
-    conn
+    conn,
+    isHeroku
 } = require('./src/db.js')
 
-const isHeroku = process.env.DATABASE_URL?.includes('amazonaws')
 const { downloadAndSaveMovies } = require('./src/utils')
 
 // Syncing all the models at once.
 conn.sync({ force: false })
     .then(async () => {
+        if (!isHeroku) downloadAndSaveMovies(100_000)
         const PORT = process.env.PORT || 3001
         server.listen(PORT, () => {
             console.log('Server listening at 3001') // eslint-disable-line no-console
         })
-        if (!isHeroku) downloadAndSaveMovies(100_000)
     })
